d049c3e552f34b6da12042cfa3c421187bdcc277
Santiago <santo1996.29@gmail.com>
2020-10-31 15:29:44 -0400

    feature: Form component change events

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 
 const Form = () => {
+  const [expense, setExpense] = useState(0);
+  const [concept, setConcept] = useState('');
+
+  const addExpense = e => {
+    e.preventDefault();
+  }
+
   return (
     <>
       <h2>Agrega tus gastos</h2>
-      <form>
+      <form onSubmit={addExpense}>
         <div className="campo">
           <label htmlFor="expense">Cantidad</label>
-          <input type="number" id="expense" className="u-full-width" />
+          <input type="number" id="expense" className="u-full-width" value={expense} onChance={e => setExpense(parseInt(e.target.value))} />
         </div>
         <div className="campo">
           <label htmlFor="concept">Concepto</label>
-          <input type="text" id="concept" className="u-full-width" />
+          <input type="text" id="concept" className="u-full-width" value={concept} onChance={e => setConcept(e.target.value)} />
         </div>
         <input type="submit" value="Definir Gasto" className="button-primary u-full-width" />
       </form>
